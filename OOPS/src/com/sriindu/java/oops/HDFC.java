cd1ec71b389004ad7936ec4780e610cb0b7eab73
kalya <badhavathkalyan08@gmail.com>
2025-04-30 12:38:29 +0530

    Interface java program

---
+++ OOPS/src/com/sriindu/java/oops/HDFC.java
@@ -0,0 +1,9 @@
+package com.sriindu.java.oops;
+
+public class HDFC implements RBI{
+	
+	public void rateOfIntrest() {
+		System.out.println("HDFC rate of intrest 7.0%");
+	}
+
+}
---
+++ OOPS/src/com/sriindu/java/oops/SBI.java
@@ -0,0 +1,7 @@
+package com.sriindu.java.oops;
+
+public class SBI implements RBI {
+	public void rateOfIntrest() {
+		System.out.println("SBI rate of intrest 6.7%");
+	}
+}
