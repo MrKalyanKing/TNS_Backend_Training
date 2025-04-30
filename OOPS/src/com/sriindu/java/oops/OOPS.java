6c04d600bd0db83a10eea39e6e3060286796e3b0
kalya <badhavathkalyan08@gmail.com>
2025-04-30 12:38:29 +0530

    Interface java program

---
+++ OOPS/src/com/sriindu/java/oops/OOPS.java
@@ -0,0 +1,13 @@
+package com.sriindu.java.oops;
+
+public class OOPS {
+
+	public static void main(String[] args) {
+		RBI r=new SBI();
+		r.rateOfIntrest();
+		RBI h=new HDFC();
+		h.rateOfIntrest();
+
+	}
+
+}
---
+++ OOPS/src/com/sriindu/java/oops/RBI.java
@@ -0,0 +1,8 @@
+package com.sriindu.java.oops;
+
+public interface RBI {
+	int DEPOSIT_LIMIT=250000;
+	int WITHDRAW=10000;
+	void rateOfIntrest();
+
+}
