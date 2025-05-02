8cbe9b391a5d2a30286afd9099675f1f1dccf0ef
kalya <badhavathkalyan08@gmail.com>
2025-05-02 22:08:21 +0530

    Operators in Java

---
+++ Operators/src/com/sriindu/java/operator/Operators.java
@@ -0,0 +1,28 @@
+package com.sriindu.java.operator;
+
+public class Operators {
+  
+	 public static void main(String args[]) {
+		 int x=2;
+		 int y=5;
+		 int exp1 = (x * y / x);
+		 int exp2 = (x * (y / x));
+		 
+		 System.out.print(exp1 );
+		 System.out.print(exp2);
+
+//		 int y;
+//		 float ans= a/b;
+//		 System.out.println(ans);
+//		 System.out.println(a%b);
+//		 y=++a;
+//		 System.out.println(a);
+//		 System.out.println(y);
+//		 y=a++;
+//		 System.out.println(a);
+//		 System.out.println(y);
+		 
+//		 logical operator
+		 
+	 }
+}
